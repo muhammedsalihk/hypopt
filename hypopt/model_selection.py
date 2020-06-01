d2bd965ca552c48d1f8fff82e5baaf41fb1d27d2
Muhammed Salih <64136574+muhammedsalihk@users.noreply.github.com>
2020-06-01 07:12:10 +0530

    Update model_selection.py

--- hypopt/model_selection.py
+++ hypopt/model_selection.py
@@ -159,7 +159,7 @@ def _run_thread_job(model_params):  # pragma: no cover
          #   score = scoring(model, job_params["X_val"], job_params["y_val"])
         # You provided a string specifying the metric, e.g. 'accuracy'
         #else:
-        if scoring == None:
+        if scoring is None:
             scoring = 'accuracy'
         score = _compute_score(
                 model = model,
